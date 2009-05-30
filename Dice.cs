7d486f9fc86e4a98bc673f8a2a4d4f45d4a689ac
Will Rogers <wjrogers@gmail.com>
2009-05-30 15:50:46 -0400

    Add dice!

---
+++ Dice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HeavyDuck.Dnd
+{
+    public static class Dice
+    {
+        private static readonly Random m_random = new Random();
+
+        public static int Roll(int count, int sides)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < count; ++i)
+                sum += m_random.Next(sides) + 1;
+
+            return sum;
+        }
+    }
+}
